c07966ce3cbcf9aaf709feeaed723d01946f0057
nk3 <nk3@TOSH105966.internal.sanger.ac.uk>
2015-11-04 15:34:57 +0000

    large scale testing completed

--- PhenStat/PhenStatSD/testing CATEGORICAL.R
+++ PhenStat/PhenStatSD/testing CATEGORICAL.R
@@ -14,7 +14,7 @@ library(MASS)
 library(logistf)
 library(lme4)
 
-setwd("C:\\git2\\IMPC_statsWorkingGroup\\PhenStat\\PhenStatSource\\R")
+setwd("U:\\git\\stats_working_group3\\PhenStat\\PhenStatSource\\R")
 source("classes.R")
 #source("classification.R")
 source("diagnostictest.R")
@@ -33,7 +33,7 @@ source("transformation.R")
 source("vectorOutput.R")
 
 
-setwd("C:\\git2\\IMPC_statsWorkingGroup\\PhenStat\\PhenStatSD")
+setwd("U:\\git\\stats_working_group3\\PhenStat\\PhenStatSD")
 source("testDataset.R")
 source("vectorOutputSD.R")
 source("MMFrameworkAssessSD.R")
@@ -65,11 +65,103 @@ result2 <- testDataset(phenList=test2,depVariable="Skull.Shape",method="SD_categ
 vectorOutputSD(result3)
 
 debug(vectorOutputSD)
-#problem with 
 
-playing1 <- analysisResults(result3)  
-playing1$SDmodel.output.summary
 
-playing1 <- analysisResults(result2)  
-playing1$SDmodel.output.summary
+###large scale testing
+###Load data into R for analysis
+setwd("Y:\\Natasha\\Categorical variables\\Logistic Regression - assessing genotype effects\\large scale testing")
+data1=read.csv("Oct29_2014_Xray_B6N_MGPSelect_Oor1.csv")
+
+#build an index of KO to drive analysis
+Index1=read.csv("Index.csv")
+
+library(plyr)
+
+countDataPoints<-function(dataset, depVariable){
+	print(sum(is.finite(dataset[ , depVariable])))
+}
+
+
+XrayVariableList=c("Number.Of.Thoracic.Vertebrae","Number.Of.Lumbar.Vertebrae"  , "Number.Of.Pelvic.Vertebrae","Number.Of.Caudal.Vertebrae"    
+		, "Transitional.Vertebrae" , "Shape.Of.Vertebrae"  , "Fusion.Of.Vertebrae", "Processes.On.Vertebrae"  , "Maxilla"  , "Zygomatic.Bone", "Number.Of.Cervical.Vertebrae","Skull.Shape"        
+		, "Number.Of.Ribs.Right", "Number.Of.Ribs.Left"  , "Shape.Of.Ribcage" , "Shape.Of.Ribs"    , "Rib.Fusions"  , "Clavicle"  , "Scapula"  ,"Humerus"                       
+		, "Radius"  , "Ulna"    ,"Pelvis"   , "Femur"  , "Tibia" , "Fibula"   , "Joints" , "Shape.Of.Spine"    , "Teeth", "Mandible"                     
+		,"Number.Of.Digits" ,"Digit.Integrity"  , "Syndactylism" , "Polysyndactylism"   ,"Brachydactylism" , "Kyphosis" , "Lordosis" ,"Scoliosis", "Spinous.Processes" , "Transverse.Processes"  , "Fusion.Processes"              
+		, "Caudal.Processes"   , "Cervical.Processes"   , "Lumbar.Processes"   , "Sacral.Processes"  ,"Thoracic.Processes"  ) 		
+
+
+data1=read.csv("Oct29_2014_Xray_B6N_MGPSelect_Oor1.csv")
+
+#PhenStat wrapper to generate output
+
+
+LR_Analysis<-function(df, GenotypeInterest, GenotypeName , BatchName, Colony.Prefix,GeneString, VariableInterest ){
+	test=PhenList(df, testGenotype=GenotypeInterest, refGenotype="WT",dataset.colname.sex="Gender",dataset.values.male="Male", 
+			dataset.values.female="Female",  dataset.clean=TRUE, dataset.colname.batch=BatchName,  dataset.colname.genotype=GenotypeName)
+	result <- testDataset(test,depVariable=VariableInterest, method="SD_categorical")
+	output=vectorOutputSD(result)
+	output=c(output, GeneString, Colony.Prefix)
+	
+	return(output)
+	
+}
+
+WT=subset(data1, data1$Genotype2=="WT")
+KO=subset(data1, data1$Genotype2!="WT")
+
+LR_Acrosslines<-function(controlData, knockoutData, IndexFile, OutputName){
+	
+	#empty object to colleate results too
+	LR_Output=c()
+	
+	IndexLength=length(IndexFile[ ,2])
+	TestsToRun=c(1:IndexLength)
+	
+	for (bob in TestsToRun){
+		
+		#identify the control mice and select them
+		ColonyPrefix=IndexFile[bob, 2]
+		GeneName=IndexFile[bob, 3]
+		Bob_zygosity=IndexFile[bob, 5]
+		
+		print(ColonyPrefix)
+		print(GeneName)
+		print(Bob_zygosity)		
+		
+		#assemble knockout data based on Index criteria
+		
+		KO_Data_bob=subset(knockoutData, knockoutData[ ,"Colony.Prefix"]==as.character(ColonyPrefix) & knockoutData[ ,"GeneName2"]==as.character(GeneName)& knockoutData[ ,"Genotype2"]==as.character(Bob_zygosity))
+		
+		#bind - but need same columns
+		dataForLR=rbind(KO_Data_bob, controlData)
+		
+		for(apple in XrayVariableList){
+			#using zygosity as genotype for study
+			#run the LR analysis
+			tryCatch(
+					{output=LR_Analysis(dataForLR, GenotypeInterest=as.character(Bob_zygosity), GenotypeName="Genotype2" , BatchName="Assay.Date", Colony.Prefix=ColonyPrefix, GeneString=GeneName,VariableInterest=apple )
+						
+						print(ColonyPrefix)
+						print(Bob_zygosity)
+						print(output)
+						
+						LR_Output=rbind(LR_Output, output)},					
+					
+					error=function(e){
+						
+						filename=paste(paste(bob,"FailureLRmethod",Bob_zygosity,ColonyPrefix,  sep="_"), "csv", sep=".")
+						write.csv(x=LR_Output, file=filename)
+					})	
+			
+		}
+		
+	}
+	write.csv(LR_Output, paste(OutputName, "csv", sep="."))				
+}
+
+
+debug(LR_Acrosslines)
+undebug(LR_Analysis)
+LR_Acrosslines(controlData=WT, knockoutData=KO, IndexFile=Index1, OutputName="Playing1.csv")
+
 
